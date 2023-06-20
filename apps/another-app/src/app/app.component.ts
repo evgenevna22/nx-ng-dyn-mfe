ae1e17ce585973da7a1de2337d963e0df39493f9
t.breneva <t.breneva@fbs.com>
2023-06-20 12:33:39 +0300

    init project

---
+++ apps/another-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-ng-dyn-fed-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'another-app';
+}
